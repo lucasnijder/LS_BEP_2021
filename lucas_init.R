2b5354a72c070a24ef56ff2ea661d0a7b7b463e1
lucasnijder <l.l.snijder@student.tue.nl>
2020-12-27 15:14:03 +0100

    added comments

--- lucas_init.R
+++ lucas_init.R
@@ -1,6 +1,3 @@
-# set working directory to folder with source files
-setwd("~/datascience4/BEP/R")
-
 # import requirered packages
 library(tidyverse)
 library(matlib)
@@ -21,12 +18,15 @@ library(dplyr)
 library(parallel)
 
 # import the source files
-file_list <- c('lucas_make_data.R', 'lucas_run_pca.R', 'lucas_run_spca.R', 'lucas_pc_select.R')
+file_list <- c('lucas_make_data.R',
+               'lucas_run_pca.R',
+               'lucas_run_spca.R',
+               'lucas_pc_select.R')
 for(file in file_list){
   source(file)
 }
 
-# set up parallel computing
+# open the connection to the clusters
 cores <- detectCores()
 cl <- makeCluster(cores[1]-1)
 clusterExport(cl, 'spca')
@@ -65,23 +65,49 @@ run_simulation <- function(sim_param_combs, variance_of_comps, nrFolds){
   res_comp_spca <- final_res$SPCA_CV[[2]]
   
   # return a list containing the results
-  return(c(num_mc = num_mc, p = p, error = e, n = n, ncomp = c, pca_ncomp = res_comp_pca, spca_ncomp = res_comp_spca))
+  return(c(num_mc = num_mc,
+           p = p,
+           error = e,
+           n = n,
+           ncomp = c,
+           pca_ncomp = res_comp_pca,
+           spca_ncomp = res_comp_spca))
 }
 
+# use 'tic' to check runtime
 tic('run_simulation')
-sim_param_combs <- expand.grid(seq(1, MC_reps), num_var_seq, num_comp_seq, error_seq, n_seq)
-results <- apply(X = sim_param_combs, MARGIN = 1, FUN = run_simulation, variance_of_comps=variance_of_comps, nrFolds=nrFolds)
+
+# create a list of all possible combinations of the simulation parameters
+sim_param_combs <- expand.grid(seq(1, MC_reps), 
+                               num_var_seq, 
+                               num_comp_seq, 
+                               error_seq, 
+                               n_seq)
+
+# run the simulations
+results <- apply(X = sim_param_combs,
+                 MARGIN = 1,
+                 FUN = run_simulation,
+                 variance_of_comps = variance_of_comps,
+                 nrFolds = nrFolds)
 toc()
 
+# close the connections to the cluster
 stopCluster(cl)
-print('done')
 
-data.frame(MC=results[1,],p=results[2,],error=results[3,],n=results[4,],ncomp=results[5,],pca_ncomp=results[6,],spca_ncomp=results[7,],pca_corect=results[8,],spca_correct=results[9,])
+# convert the results to a data frame for readability
+data.frame(MC = results[1,],
+           p = results[2,],
+           error = results[3,],
+           n = results[4,],
+           ncomp = results[5,],
+           pca_ncomp = results[6,],
+           spca_ncomp = results[7,])
 
-  
-# next step is to create plots?
-# or to look at if the current CV method is correct
-# and to look at which other methods are useful? e.g. KG-criterion? 
-# is parallel analysis really not an option?
+
+# # next step is to create plots?
+# # or to look at if the current CV method is correct
+# # and to look at which other methods are useful? e.g. KG-criterion?
+# # is parallel analysis really not an option?
 
 
