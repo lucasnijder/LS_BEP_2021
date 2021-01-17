186a2f3831926cc4811a2d698c17196bea58275c
lucasnijder <l.l.snijder@student.tue.nl>
2021-01-17 20:15:58 +0100

    Ready for use

--- BEP/simulation/run_simulation.R
+++ BEP/simulation/run_simulation.R
@@ -1,25 +1,3 @@
-library(tictoc)
-library(sparsepca)
-library(parallel)
-library(tidyverse)
-library(ggplot2)
-library(reshape2)
-library(MASS)
-library(ggpubr)
-library(grid)
-library(sjmisc)
-library(nFactors)
-library(ggthemes)
-library(glue)
-library(ggtext)
-
-source('PA_method.R')
-source('AF_method.R')
-source('OC_method.R')
-source('KG_method.R')
-source('CV_method.R')
-source('Schipper_Deun_make_data.R')
-source('SPCA_cv.R')
 
 cl <- makeCluster(7)
 clusterExport(cl, 'spca')
@@ -29,8 +7,8 @@ c_seq <- 3
 p_seq <- c(10, 20)
 error_seq <- c(0.01,0.1,0.15)
 
-num_mc <- 50
-run_type <- 'spca'
+num_mc <- 2
+run_type <- 'pca'
 
 combs <- expand.grid(n_seq, c_seq, p_seq, error_seq)
 
@@ -180,18 +158,18 @@ AF_pred_comps <- res_mc[[4]]
 KG_pred_comps <- res_mc[[5]]
 CV_pred_comps <- res_mc[[6]]
 
-write.table(res, sprintf('%s_res.csv',run_type), append = FALSE, sep = ",", dec = ".",
-            row.names = FALSE, col.names = TRUE)
-write.table(PA_pred_comps, sprintf('%s_PA_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
-            row.names = FALSE, col.names = TRUE)
-write.table(OC_pred_comps, sprintf('%s_OC_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
-            row.names = FALSE, col.names = TRUE)
-write.table(AF_pred_comps, sprintf('%s_AF_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
-            row.names = FALSE, col.names = TRUE)
-write.table(KG_pred_comps, sprintf('%s_KG_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
-            row.names = FALSE, col.names = TRUE)
-write.table(CV_pred_comps, sprintf('%s_CV_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
-            row.names = FALSE, col.names = TRUE)
+# write.table(res, sprintf('%s_res.csv',run_type), append = FALSE, sep = ",", dec = ".",
+#             row.names = FALSE, col.names = TRUE)
+# write.table(PA_pred_comps, sprintf('%s_PA_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
+#             row.names = FALSE, col.names = TRUE)
+# write.table(OC_pred_comps, sprintf('%s_OC_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
+#             row.names = FALSE, col.names = TRUE)
+# write.table(AF_pred_comps, sprintf('%s_AF_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
+#             row.names = FALSE, col.names = TRUE)
+# write.table(KG_pred_comps, sprintf('%s_KG_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
+#             row.names = FALSE, col.names = TRUE)
+# write.table(CV_pred_comps, sprintf('%s_CV_pred_comps.csv',run_type), append = FALSE, sep = ",", dec = ".",
+#             row.names = FALSE, col.names = TRUE)
 
 toc()
 
